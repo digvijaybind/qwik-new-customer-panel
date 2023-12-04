d1a98055397c5e5aff072df16b12efeb2e6b95da
digvijay-md <digvijay.bind@mktdynamics.com>
2023-12-04 15:02:48 +0530

    chnages

--- components/Newcards/newCard.jsx
+++ components/Newcards/newCard.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import styles from "./newcard.module.css";
-const Newcard = () => {
+import Image from "next/image";
+const Newcard = ({Title, date, descripation, Cardimage, buttontitle}) => {
   return (
-    <div class={`flex justify-center cursor-pointer `}>
-      <div class={`${styles.Container}`}>Read More</div>
+    <div class="flex flex-col items-stretch">
+      <div class="w-full">
+        <Image
+          src={Cardimage}
+          height={250}
+          // width="100%"
+          class="w-full"
+        />
+      </div>
+      <div class="mt-[10px] mb-[10px] text-[#15264C] font-normal">{Title}</div>
+      <div class="mb-[10px] mt-[15px] text-[#848484] font-normal text-[15px]">
+        {date}
+      </div>
+      <div class="mb-[30px] text-[#848484] font-normal text-[15px] pr-[15px] mr-[20px]">
+        {descripation}
+      </div>
+      <div class={`flex justify-center cursor-pointer `}>
+        <div class={`${styles.Container}`}>{buttontitle}</div>
+      </div>
     </div>
   );
 };
