d029cc5e3568da69955ecc6d46270193d5371c15
digvijay-md <digvijay.bind@mktdynamics.com>
2023-12-04 16:42:56 +0530

    chnages

--- components/Newcards/newCard.jsx
+++ components/Newcards/newCard.jsx
@@ -1,28 +1,42 @@
 import React from "react";
 import styles from "./newcard.module.css";
 import Image from "next/image";
+import Slider from "react-slick";
 const Newcard = ({Title, date, descripation, Cardimage, buttontitle}) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000, // Adjust the autoplay speed as needed
+  };
   return (
-    <div class="flex flex-col items-stretch">
-      <div class="w-full">
-        <Image
-          src={Cardimage}
-          height={250}
-          // width="100%"
-          class="w-full"
-        />
+    <Slider {...settings}>
+      <div class="flex flex-col items-stretch">
+        <div class="w-full">
+          <Image
+            src={Cardimage}
+            height={250}
+            // width="100%"
+            class="w-full"
+          />
+        </div>
+        <div class="mt-[10px] mb-[10px] text-[#15264C] font-normal">
+          {Title}
+        </div>
+        <div class="mb-[10px] mt-[15px] text-[#848484] font-normal text-[15px]">
+          {date}
+        </div>
+        <div class="mb-[30px] text-[#848484] font-normal text-[15px] pr-[15px] mr-[20px]">
+          {descripation}
+        </div>
+        <div class={`flex justify-center cursor-pointer `}>
+          <div class={`${styles.Container}`}>{buttontitle}</div>
+        </div>
       </div>
-      <div class="mt-[10px] mb-[10px] text-[#15264C] font-normal">{Title}</div>
-      <div class="mb-[10px] mt-[15px] text-[#848484] font-normal text-[15px]">
-        {date}
-      </div>
-      <div class="mb-[30px] text-[#848484] font-normal text-[15px] pr-[15px] mr-[20px]">
-        {descripation}
-      </div>
-      <div class={`flex justify-center cursor-pointer `}>
-        <div class={`${styles.Container}`}>{buttontitle}</div>
-      </div>
-    </div>
+    </Slider>
   );
 };
 
