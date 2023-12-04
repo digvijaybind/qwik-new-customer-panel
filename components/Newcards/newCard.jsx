f2a367b317f5122fc2e3a491bd9ccfa26afbe3b4
PANKAJ BARMAN <pankajbarman000@gmail.com>
2023-12-04 17:31:38 +0530

    home page ui changes

--- components/Newcards/newCard.jsx
+++ components/Newcards/newCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./newcard.module.css";
 import Image from "next/image";
 import Slider from "react-slick";
-const Newcard = ({Title, date, descripation, Cardimage, buttontitle}) => {
+const Newcard = ({ Title, date, descripation, Cardimage, buttontitle }) => {
   const settings = {
     dots: true,
     infinite: true,
@@ -14,7 +14,7 @@ const Newcard = ({Title, date, descripation, Cardimage, buttontitle}) => {
   };
   return (
     <Slider {...settings}>
-      <div class="flex flex-col items-stretch">
+      <div class="flex flex-col items-stretch border border-[#ececec] h-full">
         <div class="w-full">
           <Image
             src={Cardimage}
@@ -23,17 +23,13 @@ const Newcard = ({Title, date, descripation, Cardimage, buttontitle}) => {
             class="w-full"
           />
         </div>
-        <div class="mt-[10px] mb-[10px] text-[#15264C] font-normal">
-          {Title}
-        </div>
-        <div class="mb-[10px] mt-[15px] text-[#848484] font-normal text-[15px]">
-          {date}
-        </div>
-        <div class="mb-[30px] text-[#848484] font-normal text-[15px] pr-[15px] mr-[20px]">
-          {descripation}
-        </div>
-        <div class={`flex justify-center cursor-pointer `}>
-          <div class={`${styles.Container}`}>{buttontitle}</div>
+        <div className="px-5 pb-4">
+          <div class="mt-[10px] mb-[10px] text-[#15264C] font-normal">{Title}</div>
+          <div class="mb-[10px] mt-[15px] text-[#848484] font-normal text-[15px]">{date}</div>
+          <div class="mb-[30px] text-[#848484] font-normal text-[14px] pr-[15px] mr-[20px]">{descripation}</div>
+          <div class={`flex justify-center cursor-pointer `}>
+            <div class={`${styles.Container}`}>{buttontitle}</div>
+          </div>
         </div>
       </div>
     </Slider>
