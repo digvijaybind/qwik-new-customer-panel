a6e022ebd583d12cec49cf0904dedc4e79976595
digvijaybind <binddigvijay123@gmail.com>
2024-03-26 15:57:04 +0400

    changes in cards

--- pages/aviapage/[id].js
+++ pages/aviapage/[id].js
@@ -37,6 +37,96 @@ const DedicatedeHeader = () => {
   );
 };
 
+// const JourneyDetails = ({
+//   aircraft,
+//   selectedCurrency,
+//   handleCurrencyChange,
+//   totalCost,
+//   data,
+// }) => {
+//   return (
+//     <>
+//       <h3 className="font-bold">
+//         {data?.from} to {data?.to}
+//       </h3>
+//       <p className="font-bold text-[0.85rem]">
+//         {data?.specificAircraft?.totalTime
+//           ? data?.specificAircraft?.totalTime?.toFixed(2)
+//           : '-'}{' '}
+//         hrs
+//       </p>
+//       <p className="my-3">
+//         1 Passengers |{' '}
+//         {aircraft?.specificAircraft?.operator?.date &&
+//           moment(aircraft?.specificAircraft?.operator?.date)?.format(
+//             'DD MMM, YYYY'
+//           )}
+//       </p>
+//       <div className="border-b border-t my-2">
+//         <div className="flex items-center justify-between py-3">
+//           <span>Flying Cost</span>
+//           <span>
+//             {currencySymbols[selectedCurrency]}
+//             {totalCost}
+//           </span>
+//         </div>
+//         {/* <p className="flex justify-between py-3">
+//           <span>GST (18%)</span>
+//           <span>₹ 2,02,719</span>
+//         </p> */}
+//       </div>
+//       <div className="flex justify-between items-center py-1 font-bold">
+//         <span>Estimated Cost</span>
+//         <div className="text-base flex items-center">
+//           <select
+//             id="currencySelector"
+//             value={selectedCurrency}
+//             onChange={handleCurrencyChange}
+//             className="border-solid border-2 border-black rounded-md text-xs"
+//           >
+//             {Object.keys(currencySymbols)?.map((currency, index) => {
+//               return (
+//                 <option value={currency} key={'currency-item' + index}>
+//                   {currency}
+//                 </option>
+//               );
+//             })}
+//           </select>
+
+//           <div className="flex flex-row items-end ml-2">
+//             {currencySymbols[selectedCurrency]}
+//             <div className=" font-extrabold"> {totalCost}</div>
+//           </div>
+//         </div>
+//       </div>
+//       <div className="rounded-md bg-gray-300 p-2 my-4">
+//         <span className="font-bold">Qwiklif</span>, helps in your emergency by
+//         providing private air ambulance at affordable Price.
+//       </div>
+//       <div className="flex flex-col mt-5">
+//         <input
+//           name="username"
+//           className="border-b mb-5 px-2 py-1"
+//           placeholder="Your Name"
+//         />
+//         <input
+//           name="username"
+//           className="border-b mb-5 px-2 py-1"
+//           placeholder="Phone"
+//         />
+//         <input
+//           name="username"
+//           className="border-b mb-5 px-2 py-1"
+//           placeholder="Email"
+//         />
+//         <button className="bg-primary rounded-md font-medium p-2 text-sm mt-2">
+//           Enquire Now
+//         </button>
+//       </div>
+//     </>
+//   );
+// };
+
 const JourneyDetails = ({
   aircraft,
   selectedCurrency,
@@ -100,9 +190,7 @@ const JourneyDetails = ({
         </div>
       </div>
       <div className="rounded-md bg-gray-300 p-2 my-4">
-        <span className="font-bold">Qwiklif</span>, helps in your emergency by
-        providing private air ambulance at affordable Price.
-      </div>
+        <span className="font-bold">Qwiklif</span>      </div>
       <div className="flex flex-col mt-5">
         <input
           name="username"
@@ -126,6 +214,8 @@ const JourneyDetails = ({
     </>
   );
 };
+
+//dedicatedHeader
 
 const FlightImages = ({ aircraftType }) => {
   const [slides, setSlides] = useState([]);
@@ -279,23 +369,54 @@ const Flightchallenger605 = () => {
     </>
   );
 };
+// const FlightDetails = ({ aircraftData, data }) => {
+//   return (
+//     <div
+//       className="rounded-md p-5 text-[0.9rem]"
+//       style={{ boxShadow: 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px' }}
+//     >
+//       <h2 className="text-base font-bold">Flight Details</h2>
+//       <div className="mt-2 mb-3 py-3 border-b-2 border-gray-100">
+//         <p className="flex justify-between text-[0.85rem]"></p>
+
+//         <div className="flex justify-between items-center w-full mt-3">
+//           <span className="font-bold text-base">{data?.from}</span>
+//           <div className="relative">
+//             <div className="bg-gray-300 h-[2px] sm:w-36 w-72">
+//               <IoIosAirplane className="text-primary bg-primary/20 rounded-full p-1 text-2xl absolute left-1/2 top-1/2 -translate-y-1/2" />
+//             </div>
+//           </div>
+//           <span className="font-bold text-base">{data?.to}</span>
+//         </div>
+//         <p className="flex justify-between">
+//           <span className="max-w-40 text-start">{data?.from}</span>
+//           <span className="max-w-40 text-end">{data?.to}</span>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
+
 const FlightDetails = ({ aircraftData, data }) => {
   return (
     <div
       className="rounded-md p-5 text-[0.9rem]"
-      style={{ boxShadow: 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px' }}
+      style={{ boxShadow: '#000 0px 0px 10px 0px' }}
     >
       <h2 className="text-base font-bold">Flight Details</h2>
-      <div className="mt-2 mb-3 py-3 border-b-2 border-gray-100">
-        <p className="flex justify-between text-[0.85rem]">
+      <div className="mt-2 mb-3 py-3  border-gray-100">
+        {/* <p className="flex justify-between text-[0.85rem]">
           <span>3/30/2024</span>
           <span>3/30/2024</span>
-        </p>
-
+        </p> */}
+        {/* <p className="flex justify-between text-[0.85rem]">
+          <span>9:50:00 PM</span>
+          <span>11:00:00 PM</span>
+        </p> */}
         <div className="flex justify-between items-center w-full mt-3">
           <span className="font-bold text-base">{data?.from}</span>
           <div className="relative">
-            <div className="bg-gray-300 h-[2px] sm:w-36 w-72">
+            <div className="bg-gray-300 flex flex-col items-center h-[2px] sm:w-36 w-72">
               <IoIosAirplane className="text-primary bg-primary/20 rounded-full p-1 text-2xl absolute left-1/2 top-1/2 -translate-y-1/2" />
             </div>
           </div>
@@ -306,9 +427,126 @@ const FlightDetails = ({ aircraftData, data }) => {
           <span className="max-w-40 text-end">{data?.to}</span>
         </p>
       </div>
+      {/* <div className="my-3 py-3 border-b-2 border-gray-100">
+        <p className="flex justify-between text-[0.85rem]">
+          <span>3/30/2024</span>
+          <span>3/30/2024</span>
+        </p>
+        <p className="flex justify-between text-[0.85rem]">
+          <span>9:50:00 PM</span>
+          <span>11:00:00 PM</span>
+        </p> */}
+      {/* <div className="flex justify-between items-center w-full mt-3">
+          <span className="font-bold text-base">Pune</span>
+          <div className="relative">
+            <div className="bg-gray-300 h-[2px] sm:w-36 w-72">
+              <IoIosAirplane className="text-primary bg-primary/20 rounded-full p-1 text-2xl absolute left-1/2 top-1/2 -translate-y-1/2" />
+            </div>
+          </div>
+          <span className="font-bold text-base">Mumbai</span>
+        </div> */}
+      {/* <p className="flex justify-between">
+          <span className="max-w-40 text-start">Lohegaon Airport</span>
+          <span className="max-w-40 text-end">
+            Chhatrapati Shivaji International Airport
+          </span>
+        </p>
+      </div> */}
+      {/* <div className="my-3 py-3 border-b-2 border-gray-100">
+        <p className="flex justify-between text-[0.85rem]">
+          <span>3/30/2024</span>
+          <span>3/30/2024</span>
+        </p>
+        <p className="flex justify-between text-[0.85rem]">
+          <span>9:50:00 PM</span>
+          <span>11:00:00 PM</span>
+        </p>
+        <div className="flex justify-between items-center w-full mt-3">
+          <span className="font-bold text-base">Pune</span>
+          <div className="relative">
+            <div className="bg-gray-300 h-[2px] sm:w-36 w-72">
+              <IoIosAirplane className="text-primary bg-primary/20 rounded-full p-1 text-2xl absolute left-1/2 top-1/2 -translate-y-1/2" />
+            </div>
+          </div>
+          <span className="font-bold text-base">Mumbai</span>
+        </div>
+        <p className="flex justify-between">
+          <span className="max-w-40 text-start">Lohegaon Airport</span>
+          <span className="max-w-40 text-end">
+            Chhatrapati Shivaji International Airport
+          </span>
+        </p>
+      </div> */}
     </div>
   );
 };
+
+// const CostDetails = ({
+//   selectedCurrency,
+//   handleCurrencyChange,
+//   totalCost,
+//   makePayment,
+// }) => {
+//   return (
+//     <div
+//       className="flex flex-col rounded-md p-5 text-[0.9rem]"
+//       style={{ boxShadow: 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px' }}
+//     >
+//       <h2 className="text-base font-bold mb-4">Cost Details</h2>
+//       <div className="flex flex-col gap-3 text-[0.9rem]">
+//         <p className="flex justify-between">
+//           <span>Flight Cost </span>
+//           <span>
+//             {currencySymbols[selectedCurrency]}
+//             {totalCost}
+//           </span>
+//         </p>
+//         {/* <p className="flex justify-between">
+//           <span>Airport handling charges</span>
+//           <span>₹ 1,23,000</span>
+//         </p>
+//         <p className="flex justify-between">
+//           <span>Sub total</span>
+//           <span>₹ 11,26,217</span>
+//         </p>
+//         <p className="flex justify-between">
+//           <span>GST (18%)</span>
+//           <span>₹ 2,02,719</span>
+//         </p> */}
+//         <div className="flex justify-between items-center">
+//           <span>Estimated cost</span>
+//           <div className="font-bold text-base flex items-center">
+//             <select
+//               id="currencySelector"
+//               value={selectedCurrency}
+//               onChange={handleCurrencyChange}
+//               className="border-solid border-2 border-black rounded-md text-xs"
+//             >
+//               {Object.keys(currencySymbols)?.map((currency, index) => {
+//                 return (
+//                   <option value={currency} key={'currency-item' + index}>
+//                     {currency}
+//                   </option>
+//                 );
+//               })}
+//             </select>
+
+//             <div className="flex flex-row items-end ml-2">
+//               {currencySymbols[selectedCurrency]}
+//               <div className=" font-extrabold"> {totalCost}</div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//       <button
+//         className="border border-primary rounded-md p-2 text-sm mt-10 hover:bg-primary hover:text-white shadow-lg shadow-primary/70 bg-primary text-white"
+//         onClick={() => alert('hii')}
+//       >
+//         Pay Now
+//       </button>
+//     </div>
+//   );
+// };
 
 const CostDetails = ({
   selectedCurrency,
@@ -318,30 +556,19 @@ const CostDetails = ({
 }) => {
   return (
     <div
-      className="flex flex-col rounded-md p-5 text-[0.9rem]"
-      style={{ boxShadow: 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px' }}
+      className={`flex flex-col rounded-md p-5 text-[0.9rem] shadow lg ${styles.Shadow}`}
+      style={{ boxShadow: '#000 0px 0px 10px 0px' }}
     >
       <h2 className="text-base font-bold mb-4">Cost Details</h2>
       <div className="flex flex-col gap-3 text-[0.9rem]">
-        <p className="flex justify-between">
+        <div className="flex justify-between">
           <span>Flight Cost </span>
           <span>
             {currencySymbols[selectedCurrency]}
             {totalCost}
           </span>
-        </p>
-        {/* <p className="flex justify-between">
-          <span>Airport handling charges</span>
-          <span>₹ 1,23,000</span>
-        </p>
-        <p className="flex justify-between">
-          <span>Sub total</span>
-          <span>₹ 11,26,217</span>
-        </p>
-        <p className="flex justify-between">
-          <span>GST (18%)</span>
-          <span>₹ 2,02,719</span>
-        </p> */}
+        </div>
+
         <div className="flex justify-between items-center">
           <span>Estimated cost</span>
           <div className="font-bold text-base flex items-center">
@@ -367,10 +594,7 @@ const CostDetails = ({
           </div>
         </div>
       </div>
-      <button
-        className="border border-primary rounded-md p-2 text-sm mt-10 hover:bg-primary hover:text-white shadow-lg shadow-primary/70 bg-primary text-white"
-        onClick={() => alert('hii')}
-      >
+      <button className="border border-[#0FE7E7] text- rounded-md p-2 text-sm mt-10 hover:bg-[#0FE7E7] hover:text-white">
         Pay Now
       </button>
     </div>
@@ -476,6 +700,7 @@ const Airtransfer = () => {
   return (
     <div
       className={`px-[30px] w-[90%] my-[50px] m-auto py-[50px] bg-white rounded-[8px] shadow-lg ${styles.Shadow}`}
+      style={{ boxShadow: '#000 0px 0px 10px 0px' }}
     >
       <div className="flex flex-col px-[50px] mb-10">
         <h1 className="text-center font-bold pb-[30px] text-[black] text-[25px]">
