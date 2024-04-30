91dad2964e8adef3c4864bc06efd7295a848d680
digvijaybind <binddigvijay123@gmail.com>
2024-04-30 18:06:36 +0400

    this mobile input search baar

--- components/mobileSearch/MobileSearch.jsx
+++ components/mobileSearch/MobileSearch.jsx
@@ -3,55 +3,67 @@ import Depature from '../../public/images/inputBox/Depature.svg';
 import Destination from '../../public/images/inputBox/Destination.svg';
 import Patience from '../../public/images/inputBox/Patience.svg';
 import Phone from '../../public/images/inputBox/Phone.svg';
+import Date from '../../public/images/inputBox/Date.svg';
 import Reverse from '../../public/images/inputBox/Reverse.svg';
-import telephone from '../../public/images/inputBox/telephone.svg';
-import { FaArrowLeftLong } from 'react-icons/fa6';
+import Telephone from '../../public/images/inputBox/telephone.svg';
 import Image from 'next/image';
-
-const AutoSuggestInput = ({
-  name,
-  type,
-  placeholder,
-  onFocus,
-  value,
-  onChange,
-  className,
-}) => {
+const MobileSearch = () => {
   return (
-    <div className="flex justify-around items-center bg-[#fff]  px-2 py-1 h-[50px] w-[240px] rounded shadow-2xl  shadow-[#e2e0e0]">
-      <div className={`pr-2 ${className}`}>
-        <FaArrowLeftLong style={{ fontWeight: 'normal' }} />
+    <div className="flex flex-col items-center font-sans">
+      <div className="w-[300px] h-[60px] px-[3px] py-[5px] flex flex-row items-center border-2 border-gray-500 rounded-lg mb-3">
+        <Image src={Depature} width={44} height={24} />
+        <div className="flex flex-col items-start">
+          <label className="font-bold text-[8px] text-[#000]  text-[10px]s">
+            FROM
+          </label>
+          <div className="text-[15px] font-extrabold">Mumbai</div>
+          <p className="text-[7px]">
+            chhatrapati Shivaji International Airports ,T2
+          </p>
+        </div>
       </div>
-      <input
-        className={`bg-transparent py-3 focus:outline-none ${styles.InputText}`}
-        name={name}
-        type={type}
-        placeholder={placeholder}
-        onFocus={onFocus}
-        value={value}
-        onChange={onChange}
-      />
-    </div>
-  );
-};
-
-const MobileSearch = ({
-  handleSubmit,
-  formData,
-  handleInputChange,
-  className,
-}) => {
-  return (
-    <div className="flex flex-col">
-      <div className="flex flex-row">
-        <Image src={Depature} width={24} height={24} />
-        <div className="flex justify-start flex-col"></div>
+      <div className="w-[300px] h-[60px] px-[3px] py-[5px] flex flex-row items-center border-2 border-gray-500 rounded-lg mb-3">
+        <Image src={Destination} width={44} height={24} />
+        <div className="flex flex-col items-start">
+          <label className="font-bold text-[8px] text-[#000]  text-[10px]s">
+            To
+          </label>
+          <div className="text-[15px] font-extrabold">Dubai</div>
+          <p className="text-[7px]">Dubai international Airport</p>
+        </div>
       </div>
-      <div className=""></div>
-      <div className=""></div>
-      <div className=""></div>
-      <div className=""></div>
-      <button></button>
+      <div className="w-[300px] h-[60px] px-[3px] py-[5px] flex flex-row items-center border-2 border-gray-500 rounded-lg mb-3">
+        <Image src={Date} width={44} height={24} />
+        <div className="flex flex-col items-start">
+          <label className="font-bold text-[8px] text-[#000]  text-[10px]s">
+            Depature date
+          </label>
+          <div className="text-[15px] font-extrabold">18 APR</div>
+          <p className="text-[7px]">thus 2024</p>
+        </div>
+      </div>
+      <div className="w-[300px] h-[60px] px-[3px] py-[5px] flex flex-row items-center border-2 border-gray-500 rounded-lg mb-3">
+        <Image src={Date} width={44} height={24} />
+        <div className="flex flex-col items-start">
+          <label className="font-bold text-[8px] text-[#000]  text-[10px]s">
+            Country Code
+          </label>
+          <div className="text-[15px] font-extrabold">+91</div>
+          <p className="text-[7px]">India</p>
+        </div>
+      </div>
+      <div className="w-[300px] h-[60px] px-[3px] py-[5px] flex flex-row items-center border-2 border-gray-500 rounded-lg mb-3">
+        <Image src={Phone} width={44} height={24} />
+        <div className="flex flex-col items-start">
+          <label className="font-bold text-[8px] text-[#000]  text-[10px]s">
+            Phone Number
+          </label>
+          <div className="text-[15px] font-extrabold">8788825286</div>
+        </div>
+      </div>
+      <button className="search bg-[#12B5E4] w-[300px] h-[41px] text-[#fff] text-center rounded-md font-extrabold">
+        Search
+      </button>
     </div>
   );
 };
