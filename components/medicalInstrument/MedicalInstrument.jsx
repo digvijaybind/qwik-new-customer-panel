e0f23bd0ca9d5c4802a0822b7d7cce6ca8bdeb1c
digvijaybind <binddigvijay123@gmail.com>
2024-05-06 14:15:40 +0400

    make new component for icon and title and descripation

---
+++ components/medicalInstrument/MedicalInstrument.jsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+import React, { useState } from 'react';
+
+const MedicalInstruments = ({ src, width, height, Title, descripation }) => {
+  return (
+    <div className="flex justify-around flex-row items-center">
+      <Image src={src} width={width} height={height} />
+      <div className="font-black text-[12px] font-sans ml-3">{Title}</div>
+      <div className="text-[12px] font-sans font-medium ml-2">
+        {descripation}
+      </div>
+    </div>
+  );
+};
+
+export default MedicalInstruments;
