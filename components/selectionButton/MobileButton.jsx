b636ae44ee31a8731b29406840a50aeda79c10dd
digvijaybind <binddigvijay123@gmail.com>
2024-05-06 14:17:07 +0400

    changes in button size and styles

--- components/selectionButton/MobileButton.jsx
+++ components/selectionButton/MobileButton.jsx
@@ -15,15 +15,6 @@ const MobileButton = ({ onChange }) => {
           Priority
         </div>
       </div>
-
-      {/* <select
-        id="countries"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[120px] h-[50px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        onChange={() => onChange()}
-      >
-        <option value="Commericial">Commericial</option>
-        <option value="Dedicated">Dedicated</option>
-      </select> */}
       <div className="relative">
         <select
           id="countries"
