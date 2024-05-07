3637f4a306cd76444dc1bb61cac201d778065343
digvijaybind <binddigvijay123@gmail.com>
2024-05-07 17:29:48 +0400

    changes in aligments of text

--- components/medicalInstrument/MedicalInstrument.jsx
+++ components/medicalInstrument/MedicalInstrument.jsx
@@ -5,7 +5,9 @@ const MedicalInstruments = ({ src, width, height, Title, descripation }) => {
   return (
     <div className="flex justify-around flex-col items-center mr-[5px]">
       <Image src={src} width={width} height={height} />
-      <div className="font-black text-[8px] font-sans ml-3 ">{Title}</div>
+      <div className="font-black text-[8px] font-sans ml-3 sm:text-center">
+        {Title}
+      </div>
       <div className="text-[12px] font-sans font-medium ml-2">
         {descripation}
       </div>
