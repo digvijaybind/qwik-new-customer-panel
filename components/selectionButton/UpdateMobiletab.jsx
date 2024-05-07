a7aceec6e83c0dd2748eb93c428f25fa2e87d36d
digvijaybind <binddigvijay123@gmail.com>
2024-05-07 17:33:22 +0400

    mobile tab issues resolved

--- components/selectionButton/UpdateMobiletab.jsx
+++ components/selectionButton/UpdateMobiletab.jsx
@@ -6,7 +6,7 @@ const UpdateMobiletab = ({ selectedTab, handleTabChange }) => {
       className={`${styles.Container} flex justify-between shadow-2xl rounded-md bg-[#fff] overflow-hidden`}
     >
       <button
-        className={`bg-white hover:bg-[#68D2F3] text-center   focus:outline-none focus:ring-2  h-10 text-sm font-bold px-2 rounded-md ${
+        className={` hover:bg-[#68D2F3] text-center   focus:outline-none focus:ring-2  h-10 text-sm font-bold px-2 rounded-md ${
           selectedTab === 'commercial'
             ? 'text-gray-900 shadow-md bg-[#68D2F3] '
             : 'text-gray-900'
@@ -17,9 +17,9 @@ const UpdateMobiletab = ({ selectedTab, handleTabChange }) => {
       </button>
 
       <button
-        className={`bg-white hover:bg-[#68D2F3] text-center  focus:outline-none focus:ring-2   h-10 text-sm font-bold px-2 rounded-md ${
+        className={` hover:bg-[#68D2F3] text-center  focus:outline-none focus:ring-2   h-10 text-sm font-bold px-2 rounded-md ${
           selectedTab === 'dedicated'
-            ? 'text-gray-900 shadow-md'
+            ? 'text-gray-900 shadow-md bg-[#68D2F3]'
             : 'text-gray-900'
         }`}
         onClick={() => handleTabChange('dedicated')}
@@ -28,6 +28,10 @@ const UpdateMobiletab = ({ selectedTab, handleTabChange }) => {
       </button>
     </div>
   );
+};
+
+UpdateMobiletab.defaultProps = {
+  selectedTab: 'commercial', // Set the default selected tab to 'commercial'
 };
 
 export default UpdateMobiletab;
