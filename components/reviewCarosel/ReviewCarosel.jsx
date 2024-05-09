4d5dbe7ebc6f0cd00b65ef29fd1830ffce7b7a7f
digvijaybind <binddigvijay123@gmail.com>
2024-05-09 14:40:08 +0400

    change in review part in landing page

--- components/reviewCarosel/ReviewCarosel.jsx
+++ components/reviewCarosel/ReviewCarosel.jsx
@@ -4,22 +4,40 @@ import Googlereview from '../googlreview/Googlereview';
 
 const ReviewCarosel = ({ interval = 4000 }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [slidesToShow, setSlidesToShow] = useState(3);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentSlide(
-        (prevSlide) => (prevSlide + 1) % Math.ceil(Review.length / 3)
+        (prevSlide) => (prevSlide + 1) % Math.ceil(Review.length / slidesToShow)
       );
     }, interval);
 
     return () => clearInterval(intervalId);
-  }, [interval]);
+  }, [interval, slidesToShow]);
 
-  const startIdx = currentSlide * 3;
-  const endIdx = Math.min(startIdx + 3, Review.length);
+  useEffect(() => {
+    const updateSlidesToShow = () => {
+      if (window.innerWidth < 768) {
+        setSlidesToShow(1);
+      } else if (window.innerWidth < 1024) {
+        setSlidesToShow(2);
+      } else {
+        setSlidesToShow(3);
+      }
+    };
+
+    updateSlidesToShow();
+
+    window.addEventListener('resize', updateSlidesToShow);
+    return () => window.removeEventListener('resize', updateSlidesToShow);
+  }, []);
+
+  const startIdx = currentSlide * slidesToShow;
+  const endIdx = Math.min(startIdx + slidesToShow, Review.length);
 
   return (
-    <div className="flex justify-center flex-col items-center h-screen">
+    <div className="flex justify-center flex-col items-center ">
       <div className="flex justify-center items-center  text-[20px] mb-10 font-black">
         Testimonials
       </div>
@@ -35,9 +53,9 @@ const ReviewCarosel = ({ interval = 4000 }) => {
           </div>
         ))}
       </div>
-      <div className="flex justify-center items-center mt-10">
-        <span className="font-black">Google &nbsp;</span> rating score:4.9 of 5
-        based on <span className="font-black"> &nbsp; 54 reviews </span>
+      <div className="flex justify-center items-center mt-10 text-center text-[18px] sm:text-[12px]">
+        <span className="font-black">Google&nbsp;</span> rating score: 4.9 of 5
+        based on <span className="font-black">&nbsp;54 reviews</span>
       </div>
     </div>
   );
