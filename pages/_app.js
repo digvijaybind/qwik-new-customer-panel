5fe4416413ddbdf5bed8cdab4155b2914843ec2e
digvijaybind <binddigvijay123@gmail.com>
2024-05-11 13:06:20 +0400

    add whatsapp library and icon in front end

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import Newfooter from '@/components/Footer/Newfooter';
 import Navbar from '@/components/header/Navbar';
 import Navbar1 from '@/components/header/Navbar1';
-
+import { FloatingWhatsApp } from 'react-floating-whatsapp';
 import { DataProvider } from '@/context/DataContext';
 import '@/styles/globals.css';
 
@@ -11,7 +11,14 @@ export default function App({ Component, pageProps }) {
       <DataProvider>
         <Navbar />
         <Component {...pageProps} />
-        
+        <FloatingWhatsApp
+          phoneNumber="+918788825286"
+          accountName="Qwiklif Air Ambulance"
+          allowEsc
+          allowClickAway
+          notification
+          notificationSound
+        />
         <Newfooter />
       </DataProvider>
     </>
