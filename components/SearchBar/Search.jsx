91a387b5034cccff685294c60e5bb5a2f8ba8a24
digvijaybind <binddigvijay123@gmail.com>
2024-05-13 14:39:46 +0400

    added new search bar

---
+++ components/SearchBar/Search.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div className=''>
+        <div className=''>
+
+        </div>
+        
+        <div className=''>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Search
