8cd2613796815b5d525ff69b2cae8ea9a7058813
digvijaybind <binddigvijay123@gmail.com>
2024-05-18 04:08:48 +0400

    changes in qwiklif

---
+++ redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import exampleSlice from './slices/exampleSlice';
+
+const store = configureStore({
+  reducer: {
+    example: exampleSlice,
+  },
+});
+
+export default store;
