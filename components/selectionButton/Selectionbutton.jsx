c0494a1d0785426f86b3a7186953f0b8a7252c92
digvijaybind <binddigvijay123@gmail.com>
2024-05-21 17:12:55 +0400

    changes in ui and front end

--- components/selectionButton/Selectionbutton.jsx
+++ components/selectionButton/Selectionbutton.jsx
@@ -3,61 +3,61 @@ import styles from './Selectionbutton.module.css';
 
 const Selectionbutton = ({ selectedTab, handleTabChange }) => {
   return (
-    <div className="grid grid-cols-3 gap-12 font-sans">
-      <div className="flex flex-col items-start justify-between w-[173px]">
-        <div className="text-[12px] font-normal font-sans">
-          Select your flight types
+    <div className="grid grid-cols-2 gap-12 font-sans">
+      <div className="flex flex-col items-start justify-between w-auto">
+        <div className="text-[12px] font-black font-sans ">
+          Book commercial airline stretcher And Charter plane stretcher
         </div>
         <div
-          className={`h-[30px] w-[120px]  font-sans text-white font-[900] text-[10px] flex justify-center  items-center text-center rounded bg-gradient-to-r from-green-400 to-green-600 ${styles.backgroundContainer}`}
+          className={`h-[30px] w-auto  font-sans text-white font-[900] text-[10px] flex justify-center  items-center text-center rounded bg-gradient-to-r from-green-400 px-[10px]  to-green-600 ${styles.backgroundContainer}`}
         >
-          ON PRIORITY
+          Click on each to know estimate price
         </div>
       </div>
-      <div
-        className={`commericial-tab h-[55px] w-[173px]  rounded flex justify-center items-center flex-col cursor-pointer bg-[#fff]  ${
-          selectedTab === 'commercial'
-            ? 'bg-[#fff] border-2  border-[#55CDF1]  text-[#55CDF1] transition-colors duration-300 hover:bg-[#f0f8ff]'
-            : 'border-2 border-[#55CDF1] hover:bg-gray-100 transition-colors duration-300'
-        } `}
-        onClick={() => handleTabChange('commercial')}
-      >
-        <label
-          className="mr-4 font-extrabold text-[13px]"
-          htmlFor="radio1"
-          name="radios"
+      <div className="flex justify-between">
+        <div
+          className={`commericial-tab h-[55px] w-[230px]  rounded-lg flex justify-center items-center flex-col cursor-pointer bg-[#fff]  mr-4 ${
+            selectedTab === 'commercial'
+              ? 'bg-[#fff] border-2  border-[#55CDF1]  text-[#55CDF1] transition-colors duration-300 hover:bg-[#f0f8ff]'
+              : 'border-2 border-[#55CDF1] hover:bg-gray-100 transition-colors duration-300'
+          } `}
+          onClick={() => handleTabChange('commercial')}
         >
-          <input
-            type="radio"
-            name="tab"
-            id="radio1"
-            value="commercial"
-            checked={selectedTab === 'commercial'}
-            onChange={() => handleTabChange('commercial')}
-            className={`mr-2 inline-block ${styles.styledRadio} font-bold `}
-          />
-          Commercial Flights
-        </label>
-        <p className="text-[#959494] text-[10px] font-sans font-normal ml-6">
-          Price will be adjusted
-        </p>
-      </div>
-      <div
-        className={`dedicated-tab  h-[55px] w-[173px] ${
-          styles.TabButton
-        }  rounded flex justify-center items-center flex-col cursor-pointer bg-[#fff]  ${
-          selectedTab === 'dedicated'
-            ? 'bg-[#fff] border-2 border-[#55CDF1] text-[#55CDF1] transition-colors duration-300 hover:bg-[#f0f8ff]'
-            : 'border-2 border-[#55CDF1] hover:bg-gray-100 transition-colors duration-300'
-        }`}
-        onClick={() => handleTabChange('dedicated')}
-      >
-        <div className="">
-          <label
-            className="mr-4 font-extrabold text-[13px]"
-            htmlFor="radio1"
-            name="radios"
-          >
+          <div className="flex flex-row items-center">
+            <input
+              type="radio"
+              name="tab"
+              id="radio1"
+              value="commercial"
+              checked={selectedTab === 'commercial'}
+              onChange={() => handleTabChange('commercial')}
+              className={`mr-2 inline-block ${styles.styledRadio} font-bold `}
+            />
+            <div className="flex flex-col">
+              <label
+                className="mr-4 font-extrabold text-[13px]"
+                htmlFor="radio1"
+                name="radios"
+              >
+                Commercial Flights
+              </label>
+              <p className="text-[#959494] text-[10px] font-sans font-normal ml-6">
+                Price will be adjusted
+              </p>
+            </div>
+          </div>
+        </div>
+        <div
+          className={`dedicated-tab  h-[55px] w-[230px] ${
+            styles.TabButton
+          }  rounded-lg flex justify-center items-center flex-col cursor-pointer bg-[#fff]  ${
+            selectedTab === 'dedicated'
+              ? 'bg-[#fff] border-2 border-[#55CDF1] text-[#55CDF1] transition-colors duration-300 hover:bg-[#f0f8ff]'
+              : 'border-2 border-[#55CDF1] hover:bg-gray-100 transition-colors duration-300'
+          }`}
+          onClick={() => handleTabChange('dedicated')}
+        >
+          <div className="flex flex-row items-center">
             <input
               type="radio"
               name="tab"
@@ -66,11 +66,19 @@ const Selectionbutton = ({ selectedTab, handleTabChange }) => {
               onChange={() => handleTabChange('dedicated')}
               className={`mr-2 inline-block ${styles.styledRadio} font-bold text-[10px]`}
             />
-            Chartered Flights
-          </label>
-          <p className="text-[#959494] text-[10px] font-sans font-normal ml-6">
-            Price will be adjusted
-          </p>
+            <div className="flex flex-col">
+              <label
+                className="mr-4 font-extrabold text-[13px]"
+                htmlFor="radio1"
+                name="radios"
+              >
+                Chartered Flights
+              </label>
+              <p className="text-[#959494] text-[10px] font-sans font-normal ml-6">
+                Price will be adjusted
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
