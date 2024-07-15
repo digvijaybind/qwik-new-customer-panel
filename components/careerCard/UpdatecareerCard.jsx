f808dfbc2d2dba6377255b0d26208767377f4d70
digvijaybind <binddigvijay123@gmail.com>
2024-07-15 18:11:39 +0400

    changes in partner with us card section

--- components/careerCard/UpdatecareerCard.jsx
+++ components/careerCard/UpdatecareerCard.jsx
@@ -3,7 +3,7 @@ import styles from "./UpdatecareerCard.module.css";
 import Image from "next/image";
 const UpdatecareerCard = ({ image, height, width, headline, descripation }) => {
   return (
-    <div className="block rounded-lg bg-white shadow-2xl dark:bg-surface-dark cursor-pointer transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-107  duration-30">
+    <div className="block rounded-lg bg-white shadow-sm dark:bg-surface-dark cursor-pointer transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-107  duration-30">
       <div className="px-[10px] py-[10px]">
         <Image
           src={image}
@@ -20,7 +20,7 @@ const UpdatecareerCard = ({ image, height, width, headline, descripation }) => {
         <p className="mb-4 text-normal font-Inter text-sm">{descripation}</p>
         <button
           type="button"
-          className="inline-block font-Inter font-bold rounded  px-6 pb-2 pt-2.5 text-xs uppercase leading-normal text-white bg-[#19c0f0] transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2"
+          className="inline-block font-Inter font-semibold rounded  px-6 pb-2 pt-2.5 text-xs uppercase leading-normal text-white bg-[#19c0f0] transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2"
           data-twe-ripple-init
           data-twe-ripple-color="light"
         >
