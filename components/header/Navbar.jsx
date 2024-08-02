ccc8cb7dcbbef5d30d4f2ef83b30c6396987ac9f
digvijaybind <binddigvijay123@gmail.com>
2024-08-02 17:28:57 +0400

    changes in front end development

--- components/header/Navbar.jsx
+++ components/header/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import HamburgerMenu from "./HamburgerMenu";
-import Updatehamburger from "./Updatehamburger";
+import UpdateHamburgerMenu from "./UpdateHamburgerMenu";
 
 const Navbar = () => {
   const [isMobileOrTablet, setIsMobileOrTablet] = useState(false);
@@ -20,7 +20,7 @@ const Navbar = () => {
     };
   }, []);
 
-  return <div>{isMobileOrTablet ? <Updatehamburger /> : <Header />}</div>;
+  return <div>{isMobileOrTablet ? <UpdateHamburgerMenu /> : <Header />}</div>;
 };
 
 export default Navbar;
