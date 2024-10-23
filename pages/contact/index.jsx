19d6dc93020ff749404b8d26af195a1be88b3d11
digvijaybind <binddigvijay123@gmail.com>
2024-10-23 19:38:41 +0400

    change in front end

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, memo } from "react";
 import styles from "./contact.module.css";
 import { EquieryApi } from "@/redux/slices/equirySlice";
 import { useDispatch } from "react-redux";
@@ -10,7 +10,6 @@ import { parsePhoneNumberFromString } from "libphonenumber-js";
 import CountryFlag from "react-country-flag";
 import Select from "react-select";
 import Swal from "sweetalert2";
-import "sweetalert2/dist/sweetalert2.min.css";
 import Image from "next/image";
 import EmailIcon from "../../public/images/contact/email.svg";
 import FacebookIcon from "../../public/images/gettouch/facebook.svg";
@@ -43,21 +42,23 @@ const socialMediaLinks = [
 ];
 
 // Contact Info Component
-const ContactInfo = ({ icon, label, value }) => (
-  <div className="flex items-center sm:items-start mt-5 cursor-pointer">
+const ContactInfo = memo(({ icon, label, value }) => (
+  <div className="flex items-center sm:items-start sm:justify-center mt-5 cursor-pointer">
     <Image src={icon} alt={label} height={40} width={40} />
-    <div className="ml-5">
-      <span className="font-barlow font-normal text-[24px]">{label}</span>
+    <div className="ml-5 sm:max-w-[150px]">
+      <span className="font-barlow font-normal text-[24px] sm:text-[18px]">
+        {label}
+      </span>
       <br />
-      <span className="font-barlow font-semibold  text-[#1E1E1E] text-[24px]">
+      <span className="font-barlow font-semibold text-[#1E1E1E] text-[24px] sm:text-[18px]">
         {value}
       </span>
     </div>
   </div>
-);
+));
 
 // Social Media Component
-const SocialMedia = ({ links }) => (
+const SocialMedia = memo(({ links }) => (
   <div className="mt-10">
     <h3 className="font-barlow font-bold text-[28px] mb-3">
       Follow Us On Social Media
@@ -75,7 +76,7 @@ const SocialMedia = ({ links }) => (
       ))}
     </div>
   </div>
-);
+));
 
 // Custom Phone Input Component
 const CustomPhoneInput = React.forwardRef(
@@ -94,7 +95,7 @@ const CustomPhoneInput = React.forwardRef(
 CustomPhoneInput.displayName = "CustomPhoneInput";
 
 // Custom Country Select Component
-const CustomCountrySelect = ({ value, onChange, ...rest }) => {
+const CustomCountrySelect = memo(({ value, onChange, ...rest }) => {
   const countries = getCountries();
   const options = countries
     .map((country) => {
@@ -133,47 +134,64 @@ const CustomCountrySelect = ({ value, onChange, ...rest }) => {
       {...rest}
     />
   );
-};
+});
 
 // Main Contact Component
-const Contact = ({ mapInframeUrl }) => {
+const Contact = () => {
   const [formData, setFormData] = useState({
-    From: "",
-    To: "",
-    Phone: "",
-    Email: "",
+    From: "Dubai",
+    To: "Sharjaha",
+    FirstName: "Digvijay",
+    LastName: "Bind",
+    Phone: "+971568835862",
+    Email: "binddigvijay123@gmail.com",
+    Message: "This is text",
   });
   const dispatch = useDispatch();
 
-  const handleChange = (e) => {
+  // Handle input changes
+  const handleChange = useCallback((e) => {
     const { name, value } = e.target;
     setFormData((prevState) => ({ ...prevState, [name]: value }));
-  };
+  }, []);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await dispatch(EquieryApi(formData));
-      if (EquieryApi.fulfilled.match(response)) {
+  // Handle form submission
+  const handleSubmit = useCallback(
+    async (e) => {
+      e.preventDefault(); // Prevent default form submission
+      try {
+        const response = await dispatch(EquieryApi(formData));
+        if (EquieryApi.fulfilled.match(response)) {
+          Swal.fire({
+            title: "Thank you for contacting us!",
+            text: "Our Team will get back to you shortly",
+            icon: "success",
+          });
+          setFormData({
+            From: "",
+            To: "",
+            FirstName: "",
+            LastName: "",
+            Phone: "",
+            Email: "",
+            Message: "",
+          }); // Clear form data
+        } else {
+          console.error("Submission error:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Error submitting form:", error);
         Swal.fire({
-          title: "Thank you for contacting us!",
-          text: "Our Team will get back to you shortly",
-          icon: "success",
+          title: "Error",
+          text: "An error occurred. Please try again.",
+          icon: "error",
         });
-        setFormData({ From: "", To: "", Phone: "", Email: "" });
-      } else {
-        console.error("Submission error:", response.statusText);
       }
-    } catch (error) {
-      console.error("Error submitting form:", error);
-      Swal.fire({
-        title: "Error",
-        text: "An error occurred. Please try again.",
-        icon: "error",
-      });
-    }
-  };
+    },
+    [dispatch, formData],
+  );
 
+  // Handle phone number changes
   const handlePhoneChange = useCallback((value) => {
     const phoneNumber = parsePhoneNumberFromString(value);
     const countryCode = phoneNumber ? phoneNumber.country : "";
@@ -183,80 +201,152 @@ const Contact = ({ mapInframeUrl }) => {
   return (
     <div className={`${styles.container} bg-[#fff]`}>
       <header
-        className="flex flex-col items-center justify-center bg-no-repeat bg-cover bg-center text-white sm:h-[20vh] h-[90vh] sm:px-10 px-36"
+        className="flex flex-col items-center justify-center bg-no-repeat bg-cover bg-center text-white sm:h-[20vh] h-[90vh] sm:px-10 px-36 sm:py-24"
         style={{ backgroundImage: "url('/images/location/Hero.svg')" }}
       >
         <div className="text-center">
-          <h1 className="font-barlow font-bold text-[64px]">Contact us</h1>
-          <p className="font-barlow text-[24px]">Home - contact us</p>
+          <h1 className="font-barlow font-bold text-[64px] sm:text-[44px]">
+            Contact us
+          </h1>
+          <p className="font-barlow text-[24px] sm:text-[18px]">
+            Home - contact us
+          </p>
         </div>
       </header>
+
       <section className="flex flex-col items-center">
-        <h2 className="font-barlow text-[54px] font-bold bg-headline-gradient text-transparent bg-clip-text mb-2">
+        <h2 className="font-barlow text-[54px] sm:text-[34px] sm:text-center font-bold bg-headline-gradient text-transparent bg-clip-text mb-2">
           Get In Touch With Qwiklif.
         </h2>
       </section>
+
       <main className="px-20 sm:px-5 py-20 sm:py-10 bg-[#f5fdff]">
-        <div className="grid grid-cols-12 sm:grid-cols-1 gap-8">
-          <section className="col-span-6 sm:col-span-1 flex flex-col items-start sm:items-center max-w-[575px]">
+        <div className="grid grid-cols-12 sm:grid-cols-1 gap-4">
+          {/* Contact Information Section */}
+          <section className="col-span-5 sm:col-span-1 flex flex-col items-start sm:items-center max-w-[575px]">
             <h1 className="font-barlow font-semibold text-[24px] sm:text-center">
               Get In Touch With Qwiklif
             </h1>
             <h2 className="font-barlow font-bold text-[54px] sm:text-[34px] bg-headline-gradient text-transparent bg-clip-text leading-tight sm:text-center mt-3">
-              Your Trusted Global Air Ambulance Provider.
+              Your Trusted Air Ambulance Service
             </h2>
-            <p className="font-barlow font-normal text-[18px] mt-4 sm:text-center">
-              In emergencies, time is crucial. Our air ambulances ensure swift
-              medical assistance.
-            </p>
-            <div className="mt-8 space-y-4 sm:space-y-6">
-              <ContactInfo
-                icon={TelephoneIcon}
-                label="Contact No."
-                value="+971 50 2825433"
-              />
-              <ContactInfo
-                icon={EmailIcon}
-                label="Email Address"
-                value="info@gmail.com"
-              />
-              <ContactInfo
-                icon={whatsapp}
-                label="WhatsApp No."
-                value="+971502825433"
-              />
-              <ContactInfo
-                icon={location}
-                label="Address"
-                value="QWIKLIF PORTAL L.L.C, Regus Dafza, 8W Level 5, Dubai Airport Freezone, Dubai."
-              />
-            </div>
+
+            <ContactInfo
+              icon={TelephoneIcon}
+              label="Call us at"
+              value="+971 568835862"
+            />
+            <ContactInfo
+              icon={EmailIcon}
+              label="Email"
+              value="info@qwiklif.com"
+            />
+            <ContactInfo icon={location} label="Location" value="Dubai, UAE" />
           </section>
-          <section className="col-span-6 sm:col-span-1">
-            <div className="bg-[#fff] px-10 py-20">
-              <div className="">Get Quote Now</div>
-            </div>
+
+          {/* Contact Form Section */}
+          <section className="col-span-7 sm:col-span-1">
+            <h1 className="font-barlow font-semibold text-[24px] text-center">
+              Leave a Message
+            </h1>
+            <form
+              onSubmit={handleSubmit}
+              className="bg-white p-6 rounded-lg shadow-md mt-6"
+            >
+              <div className="flex flex-col space-y-4">
+                <input
+                  type="text"
+                  name="FirstName"
+                  placeholder="First Name"
+                  value={formData.FirstName}
+                  onChange={handleChange}
+                  className="border border-gray-300 rounded-lg p-2"
+                  required
+                />
+                <input
+                  type="text"
+                  name="LastName"
+                  placeholder="Last Name"
+                  value={formData.LastName}
+                  onChange={handleChange}
+                  className="border border-gray-300 rounded-lg p-2"
+                  required
+                />
+                <div className="flex space-x-4">
+                  <CustomCountrySelect
+                    name="countryCode"
+                    value={formData.countryCode}
+                    onChange={(value) =>
+                      setFormData((prevState) => ({
+                        ...prevState,
+                        countryCode: value,
+                      }))
+                    }
+                  />
+                  <PhoneInput
+                    placeholder="Enter phone number"
+                    value={formData.Phone}
+                    onChange={handlePhoneChange}
+                    component={CustomPhoneInput}
+                    defaultCountry="AE"
+                    className="border border-gray-300 rounded-lg w-full"
+                    required
+                  />
+                </div>
+                <input
+                  type="email"
+                  name="Email"
+                  placeholder="Email"
+                  value={formData.Email}
+                  onChange={handleChange}
+                  className="border border-gray-300 rounded-lg p-2"
+                  required
+                />
+                <textarea
+                  name="Message"
+                  placeholder="Message"
+                  value={formData.Message}
+                  onChange={handleChange}
+                  className="border border-gray-300 rounded-lg p-2 h-32"
+                  required
+                />
+                <button
+                  type="submit"
+                  className="bg-blue-500 text-white rounded-lg p-2 hover:bg-blue-600"
+                >
+                  Send Message
+                </button>
+              </div>
+            </form>
           </section>
         </div>
       </main>
-      <div className="flex flex-col justify-center items-center mt-10  mb-5 sm:mb-0 py-5 px-20">
-        <iframe
-          src={mapInframeUrl}
-          height="450"
-          loading="lazy"
-          className="w-full mt-3 mb-3 rounded-md"
-        ></iframe>
+
+      <div className="w-full sm:w-full">
+        <Image src={Map} alt="Map" className="rounded-lg" />
       </div>
+
+      {/* Social Media Section */}
+      <SocialMedia links={socialMediaLinks} />
+
+      {/* Google Maps Section */}
+      <section className="flex flex-col items-center">
+        <h2 className="font-barlow font-semibold text-[24px] sm:text-center">
+          Where We Are
+        </h2>
+        <div className="w-full sm:w-full">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153091.072014882!2d55.20332746439264!3d25.30204658589277!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f3f39ec53ef7f%3A0xfca006f02c6a1246!2sDubai!5e0!3m2!1sen!2s!4v1632511234125!5m2!1sen!2s"
+            width="100%"
+            height="450"
+            style={{ border: "0" }}
+            allowFullScreen=""
+            loading="lazy"
+          ></iframe>
+        </div>
+      </section>
     </div>
   );
-};
-
-export const getServerSideProps = async () => {
-  const mapInframeUrl =
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3...";
-  return {
-    props: { mapInframeUrl },
-  };
 };
 
 export default Contact;
